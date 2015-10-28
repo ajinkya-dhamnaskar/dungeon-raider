4aca9736b8ef511e5be63ac86ce9f1f984d3ed82
Amruta <arkamat@indiana.edu>
2015-10-28 02:31:23 -0400

    file updated

--- app/src/main/java/com/group2/dungeonraider/controller/MainActivity.java
+++ app/src/main/java/com/group2/dungeonraider/controller/MainActivity.java
@@ -1,24 +1,22 @@
 package com.group2.dungeonraider.controller;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 import com.example.test.dungeonmainmenu.R;
-import com.group2.dungeonraider.utilities.Constants;
 
-/*
-Entry point
- */
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends Activity {
+//asdfasdfasdfasdzx
     @Override
       protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void setting(View v)
@@ -26,12 +24,43 @@ public class MainActivity extends AppCompatActivity {
         Intent i=new Intent(this,Setting.class);
         startActivity(i);
     }
-
     public void scorecard(View v)
     {
         Intent i=new Intent(this,ScoreCard.class);
         startActivity(i);
     }
+
+    public void newgame(View v)
+    {
+        Intent i=new Intent(this,NewGame.class);
+        startActivity(i);
+    }
+
+//    public void theme(View v)
+//    {
+//
+//        Spinner staticSpinner = (Spinner) findViewById(R.id.spn_theme);
+//
+//        // Create an ArrayAdapter using the string array and a default spinner
+//        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter
+//                .createFromResource(this, R.array.theme_array,
+//                        android.R.layout.simple_spinner_item);
+//
+//        // Specify the layout to use when the list of choices appears
+//        staticAdapter
+//                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//
+//        // Apply the adapter to the spinner
+//        staticSpinner.setAdapter(staticAdapter);
+//
+//
+//
+//
+//
+//        }
+
+
+
 
     public void quit(View v)
     {
@@ -39,8 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this);
 
         // set title
-        alertDialogBuilder.setTitle("Are you sure you want to quit the game");
-
+        alertDialogBuilder.setTitle("Are you sure you want to quit the game?");
 
         // set dialog message
         alertDialogBuilder
--- app/src/main/java/com/group2/dungeonraider/controller/ScoreCard.java
+++ app/src/main/java/com/group2/dungeonraider/controller/ScoreCard.java
@@ -1,44 +1,71 @@
 package com.group2.dungeonraider.controller;
 
-
 import android.app.Activity;
-import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.test.dungeonmainmenu.R;
 
-import java.util.ArrayList;
-
 /**
- * Created by Rohit on 10/25/2015.
+ * Created by Amruta on 10/27/2015.
  */
-public class ScoreCard extends Activity {
-    MediaPlayer mp1;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scoreboard);
+public class ScoreCard extends Activity{
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.activity_scoreboard);
+
+//            SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(ScoreCard.this);
+//            SharedPreferences.Editor editor = sp.edit();
+//            editor.putString("username", "CHAAVA");
+//            editor.putInt("score", 12);
+//            editor.commit();
+//
+//            String gameUsername = sp.getString("username", "default");
+//            int gameScore = sp.getInt("score", 0);
+//
+//            TextView userView = (TextView)findViewById(R.id.textViewtest1);
+//            TextView scoreView = (TextView)findViewById(R.id.textViewtest2);
+//
+//
+//
+//            scoreView.setText(gameUsername.toString());
+//            userView.setText(gameScore);
 
 
-      //  TextView scoreView = (TextView)findViewById(R.id.high_scores_list);
-        mp1= MediaPlayer.create(ScoreCard.this, R.raw.cointake);
-        mp1.start();
+//            TableLayout table = (TableLayout)findViewById(R.id.tableLayout);
+//
+//            TextView rank = (TextView)findViewById(R.id.rank);
+//            rank.setText("RANK");
+//            TextView percentage = (TextView)findViewById(R.id.percentage);
+//            percentage.setText("SCORE");
+//            TextView score = (TextView)findViewById(R.id.score);
+//            score.setText("TIME");
+//
+//            TableRow rowHeader = (TableRow)findViewById(R.id.rowHeader);
+//
+//            rowHeader.addView(rank);  //Line 39
+//            rowHeader.addView(percentage);
+//            rowHeader.addView(score);
+//
+//
+//
+//           //table.addView(rowHeader, new TableLayout.LayoutParams(Layout.getDesiredWidth(300), LayoutParams.WRAP_CONTENT));
 
-         ArrayList<String> highscores = new ArrayList<String>();
 
-    }
+        }
 
-
-
-    public void backtomain(View v)
+    public void backtomainscoreboard(View v)
     {
-
         ScoreCard.this.finish();
-}
-
+    }
 
     public void onBackPressed() {
+        Log.d("Scorecard", "onBackPressed Called");
+
         ScoreCard.this.finish();
+        return;
     }
+
 }
