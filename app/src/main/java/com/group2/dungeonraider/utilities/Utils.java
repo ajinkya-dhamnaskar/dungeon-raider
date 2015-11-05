9d031f2cf99f4da8250844b9438ac0970c73b9d3
Amruta <arkamat@indiana.edu>
2015-11-05 00:32:47 -0500

    Changes

---
+++ app/src/main/java/com/group2/dungeonraider/utilities/Utils.java
@@ -0,0 +1,22 @@
+package com.group2.dungeonraider.utilities;
+
+/**
+ * Created by Ajinkya on 11/3/2015.
+ */
+public class Utils {
+
+    public static String getTime(long milliseconds){
+        String time = null;
+
+
+        return time;
+    }
+
+    public static boolean clearGameData(){
+        boolean result = true;
+        Constants.GAME_NO_OF_BOMBS = 0;
+        Constants.GAME_NO_OF_POTIONS = 0;
+        Constants.GAME_NO_OF_KEYS = 0;
+        return result;
+    }
+}
